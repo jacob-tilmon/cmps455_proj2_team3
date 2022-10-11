920b5d1c7f634f2ed9f8c36f72dce6d287589a6a
Jacob_Tilmon_Ubuntu <C00292879@louisiana.edu>
2022-10-11 12:50:00 -0500

    added new classes for tasks 1-3, added input for user - Jacob

---
+++ src/AccessProtectedObject.java
@@ -0,0 +1,2 @@
+public class AccessProtectedObject {
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // NEED TWO OBJECT TYPES, THREADS AND OBJECTS, BOTH NEED A FEW CONSTRUCTORS
+        // IN ORDER TO BE COMPATIBLE WITH EACH TASK. - JACOB
+
+        if(args[0].equals("-S")){
+            if (args.length == 1) {
+                System.out.println("Missing Argument, Please include 1, 2, or 3 after -S");
+            }
+            else if(args[1].equals("1")){
+                //access matrix
+                accessMatrix();
+            }
+            else if(args[1].equals("2")){
+                //access list
+                accessList();
+            }
+            else if(args[1].equals("3")){
+                //capability list
+                capabilityList();
+            }
+            else{
+                System.out.println("Unexpected Argument. Please try again.");
+            }
+        }
+        else  {
+            System.out.println("Unexpected Tag. Please try again.");
+        }
     }
+    public static void accessMatrix(){
+
+    }
+    public static void accessList(){
+
+    }
+    public static void capabilityList(){
+
+    }
+
+
 }
