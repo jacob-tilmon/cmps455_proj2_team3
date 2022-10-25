a493975e62fa5a9df64a72e46695defdc122c9fe
Jacob_Tilmon_Ubuntu <C00292879@louisiana.edu>
2022-10-24 19:24:56 -0500

    added yeilding

--- src/UserThreadT3.java
+++ src/UserThreadT3.java
@@ -34,18 +34,18 @@ public class UserThreadT3 implements Runnable{
                 int requestInt = random.nextInt(2);
                 if (requestInt == 0) {
                     accessReq = "R";
-                    System.out.println("Thread[" + id + "] is making a read request on object " + X);
+                    System.out.println("Thread[" + id + "] in domain "+curDomain+" is making a read request on object " + X);
                 }
                 if (requestInt == 1) {
                     accessReq = "W";
-                    System.out.println("Thread[" + id + "] is making a write request on object " + X);
+                    System.out.println("Thread[" + id + "] in domain "+curDomain+" is making a write request on object " + X);
                 }
             } else {
                 if((N + M) - X - 1 == curDomain){
                     continue;
                 }
                 accessReq = "allow";
-                System.out.println("Thread[" + id + "] is making a domain switch to domain " + ((N + M) - X - 1));
+                System.out.println("Thread[" + id + "] in domain "+curDomain+" is making a domain switch to domain " + ((N + M) - X - 1));
             }
 
             if (myCurrentCapabilityList.get(X).contains(accessReq) ||
@@ -62,7 +62,11 @@ public class UserThreadT3 implements Runnable{
                     curDomain = (N + M) - X - 1;
             } else {
                 System.out.println("Thread[" + id + "] permission was denied.");
+                int waitTime = random.nextInt(5)+3;
+                for (int i = 0; i < waitTime; i++) Thread.yield();
             }
+            int waitTime = random.nextInt(5)+3;
+            for (int i = 0; i < waitTime; i++) Thread.yield();
             attempts--;
         }
     }
